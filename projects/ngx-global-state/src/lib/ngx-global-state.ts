081129c6cdb20696b4da4821a0a5caf67d018976
Devalfe <mdt.webmaster@gmail.com>
2025-10-02 14:38:40 -0500

    chore: initial commit from @angular/cli
         _                      _               ____ _     ___
        / \   _ __   __ _ _   _| | __ _ _ __   / ___| |   |_ _|
       / _ \ | '_ \ / _` | | | | |/ _` | '__| | |   | |    | |
      / ___ \| | | | (_| | |_| | | (_| | |    | |___| |___ | |
     /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|     \____|_____|___|

---
+++ projects/ngx-global-state/src/lib/ngx-global-state.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-global-state',
+  imports: [],
+  template: `
+    <p>
+      ngx-global-state works!
+    </p>
+  `,
+  styles: ``
+})
+export class NgxGlobalState {
+
+}
---
+++ projects/ngx-global-state/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of ngx-global-state
+ */
+
+export * from './lib/ngx-global-state';
