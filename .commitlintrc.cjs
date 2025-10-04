f965a6e24e697e2ef68b66679fbf763c736fdd69
Devalfe <mdt.webmaster@gmail.com>
2025-10-04 09:30:43 -0500

    chore(config): update semantic-release and commitlint configs, refine npm ci in pipeline

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  ignores: [(message) => message.includes('[skip ci]')],
   rules: {
     'type-enum': [
       2,
@@ -8,6 +9,8 @@ module.exports = {
     ],
     'header-max-length': [2, 'always', 200],
     'body-max-line-length': [2, 'always', 500],
+    'footer-max-line-length': [2, 'always', 500],
+    'footer-leading-blank': [2, 'always'],
     'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
   },
   prompt: {
